4ef1651089ab25a6225e515ac78c4f69047daff5
masrurprem <masrur.kuet@gmail.com>
2025-08-04 16:26:47 +0600

    vendorAuth function correction

--- src/middleware/vendorAuth.js
+++ src/middleware/vendorAuth.js
@@ -1,24 +1,25 @@
 const vendorModel = require("../models/vendorModel");
 const jwt = require("jsonwebtoken");
 //
-const vendorAuth = async (req, res, next) => {};
-try {
-  const token = req.header("Authorization").replace("Bearer", "");
-  const payload = jwt.verify(token, "abcabcabc");
-  const vendor = await vendorModel.findOne({
-    email: payload.email,
-    "tokens.token": token,
-  });
+const vendorAuth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer", "");
+    const payload = jwt.verify(token, "abcabcabc");
+    const vendor = await vendorModel.findOne({
+      email: payload.email,
+      "tokens.token": token,
+    });
 
-  if (!vendor) {
-    throw new Error();
+    if (!vendor) {
+      throw new Error();
+    }
+    // here, so vendor is authorized
+    req.token = token;
+    req.vendor = vendor;
+    next();
+  } catch (error) {
+    res.status(400).send("please authenticate");
   }
-  // here, so vendor is authorized
-  req.token = token;
-  req.vendor = vendor;
-  next();
-} catch (error) {
-  res.status(400).send("please authenticate");
-}
+};
 
 module.exports = vendorAuth;
